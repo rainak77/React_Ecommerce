20498113b934b61e4dcd88155cc98550d66b46b5
md syedul arif <raiak_77@yahoo.com>
2020-10-14 16:01:15 +0300

    update our menu item to navigate with router

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-item.style.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
-        <div className='background-image'
-            style={{ backgroundImage: `url(${imageUrl})` }}>
+
+
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+    console.log(match.url);
+    return (
+        <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+            <div className='background-image'
+                style={{ backgroundImage: `url(${imageUrl})` }}>
+            </div>
+            <div className='content'>
+                <h1 className='title'>{title.toUpperCase()}</h1>
+                <span className='subtitle'>Shop Now</span>
+            </div>
         </div>
-        <div className='content'>
-            <h1 className='title'>{title.toUpperCase()}</h1>
-            <span className='subtitle'>Shop Now</span>
-        </div>
-    </div>
-);
-export default MenuItem;
+    );
+};
+export default withRouter(MenuItem);
